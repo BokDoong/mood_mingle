6b179c0f039c776e363fd5cb640a1ad04fff7a3e
이진 <ctce7226@gmail.com>
2024-04-29 08:52:15 +0900

    feat: JWT 토큰 예외 Enum코드 추가

---
+++ src/main/java/uni/capstone/moodmingle/config/jwt/JwtException.java
@@ -0,0 +1,43 @@
+package uni.capstone.moodmingle.config.jwt;
+
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import uni.capstone.moodmingle.exception.ErrorResponse;
+
+import java.io.IOException;
+
+import static org.springframework.http.HttpStatus.*;
+
+@Getter
+@AllArgsConstructor
+public enum JwtException {
+    EXPIRED_TOKEN(UNAUTHORIZED, "만료된 토큰", "AUTH-001"),
+    WRONG_TOKEN(UNAUTHORIZED, "유효하지 않은 토큰", "AUTH-002"),
+    UNSUPPORTED_TOKEN(UNAUTHORIZED, "지원하지 않는 토큰 형식", "AUTH-003"),
+    EMPTY_TOKEN(UNAUTHORIZED, "토큰이 없음", "AUTH-004"),
+    ACCESS_DENIED(FORBIDDEN, "권한이 없음", "AUTH_005"),
+    UNKNOWN_ERROR(SERVICE_UNAVAILABLE, "알수 없는 에러", "AUTH-006");
+
+    private final HttpStatus status;
+    private final String message;
+    private final String code;
+
+    public void setResponse(HttpServletResponse response) throws IOException {
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+        response.setCharacterEncoding("UTF-8");
+        response.getWriter().write(String.valueOf(createJwtExceptionBody(this)));
+    }
+
+    private static ErrorResponse createJwtExceptionBody(JwtException exception) {
+        return ErrorResponse.builder()
+                .status(401)
+                .error(exception.getStatus().name())
+                .code(exception.getCode())
+                .message(exception.getMessage())
+                .build();
+    }
+}
