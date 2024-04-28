484cb11859c7f2e861bdc9766773e083c90229b6
이진 <ctce7226@gmail.com>
2024-04-28 15:16:27 +0900

    feat: Swagger 인증 트러블 해결을 위한 WebSecurityConfig 추가

---
+++ src/main/java/uni/capstone/moodmingle/config/WebSecurityConfig.java
@@ -0,0 +1,32 @@
+package uni.capstone.moodmingle.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class WebSecurityConfig {
+
+    private static final String[] AUTH_BLACKLIST = {};
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        // FormLogin, BasicHttp 비활성화
+        http.formLogin(AbstractHttpConfigurer::disable);
+        http.httpBasic(AbstractHttpConfigurer::disable);
+
+        // 권한 규칙 설정
+        http.authorizeHttpRequests(authorize -> authorize
+                .requestMatchers("/admin/**").hasRole("ADMIN")
+                .anyRequest().permitAll());
+
+        return http.build();
+    }
+}
