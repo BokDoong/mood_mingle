1b4b065c759c235a36efad201041e0275edd3931
이진 <ctce7226@gmail.com>
2024-05-05 10:28:25 +0900

    chore: 각 클래스, 필드, 메서드 별 주석 추가

--- src/main/java/uni/capstone/moodmingle/diary/domain/DiaryRepository.java
+++ src/main/java/uni/capstone/moodmingle/diary/domain/DiaryRepository.java
@@ -1,8 +1,15 @@
 package uni.capstone.moodmingle.diary.domain;
 
+/**
+ * Diary Repository
+ */
 public interface DiaryRepository {
 
-    // 일기장 저장
+    /**
+     * 저장
+     *
+     * @param diary Diary 객짜
+     */
     void save(Diary diary);
 
     
