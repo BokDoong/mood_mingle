e7ed3a00b5e076955b107399c52f6623eed00606
이진 <ctce7226@gmail.com>
2024-05-07 20:59:16 +0900

    feat: 애플리케이션 계층에서 Diary와 Reply가 함께 CRUD 되는 애플리케이션 서비스 단 로직 분리

--- src/main/java/uni/capstone/moodmingle/diary/application/ReplyManageService.java
+++ src/main/java/uni/capstone/moodmingle/diary/application/ReplyManageService.java
@@ -1,11 +1,13 @@
 package uni.capstone.moodmingle.diary.application;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
-import uni.capstone.moodmingle.diary.application.dto.PromptProcessingHelper;
+import uni.capstone.moodmingle.diary.application.dto.request.ReplyCreateCommand;
 import uni.capstone.moodmingle.diary.infra.dto.Message;
 
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 /**
  * LLM 과 통신하는 어댑터에 프롬프트를 전달하여 답변 받아오는 애플리케이션 서비스
@@ -16,19 +18,34 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ReplyManageService {
 
+    private final PromptProcessingFacade processingFacade;
     private final LLMClient client;
 
     /**
      * 프롬프트 전달 및 답변 받기
      *
-     * @param diaryPrompt 일기 프롬프트
-     * @param replyPrompt 답장 프롬프트
+     * @param command ReplyCreateCommand - DTO
      * @return LLM 답변
      */
-    public String replyByLetter(String diaryPrompt, String replyPrompt) {
+    @Async("AsyncExecutor")
+    public CompletableFuture<String> replyByLetter(ReplyCreateCommand command) {
         // LLM Request Message 가공
-        List<Message> prompts = PromptProcessingHelper.processReplyPrompt(diaryPrompt, replyPrompt);
+        List<Message> prompts = processingFacade.processLetterReplyPrompt(command);
         // LLMClient 에 요청
-        return client.requestLetter(prompts);
+        return CompletableFuture.completedFuture(client.requestLetter(prompts));
+    }
+
+    /**
+     * 프롬프트 전달 및 답변 받기
+     *
+     * @param command ReplyCreateCommand - DTO
+     * @return LLM 답변
+     */
+    @Async("AsyncExecutor")
+    public CompletableFuture<String> replyBySympathyPhrase(ReplyCreateCommand command) {
+        // LLM Request Message 가공
+        List<Message> prompts = processingFacade.processSympathyReplyPrompt(command);
+        // LLMClient 에 요청
+        return CompletableFuture.completedFuture(client.requestSympathyPhrase(prompts));
     }
 }
