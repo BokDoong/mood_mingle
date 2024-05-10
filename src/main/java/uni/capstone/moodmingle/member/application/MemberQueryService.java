1f364df7d5ec33ee36fec49dfe1d9096790cac47
이진 <ctce7226@gmail.com>
2024-05-10 15:43:34 +0900

    chore: 멤버 정보조회 API 관련 주석 추가

--- src/main/java/uni/capstone/moodmingle/member/application/MemberQueryService.java
+++ src/main/java/uni/capstone/moodmingle/member/application/MemberQueryService.java
@@ -8,12 +8,23 @@ import uni.capstone.moodmingle.member.application.dto.response.MemberInfo;
 import uni.capstone.moodmingle.member.domain.Member;
 import uni.capstone.moodmingle.member.domain.MemberRepository;
 
+/**
+ * Member 도메인 조회 응용 서비스
+ *
+ * @author ijin
+ */
 @Service
 @RequiredArgsConstructor
 public class MemberQueryService {
 
     private final MemberRepository memberRepository;
 
+    /**
+     * memberId => 회원정보 조회
+     *
+     * @param memberId
+     * @return MemberInfo DTO
+     */
     public MemberInfo findMember(Long memberId) {
         Member member = findMemberById(memberId);
         return new MemberInfo(member.getName(), member.getEmail());
--- src/main/java/uni/capstone/moodmingle/member/application/dto/response/MemberInfo.java
+++ src/main/java/uni/capstone/moodmingle/member/application/dto/response/MemberInfo.java
@@ -1,3 +1,9 @@
 package uni.capstone.moodmingle.member.application.dto.response;
 
+/**
+ * 회원정보 조회 DTO Record
+ *
+ * @param name 이름
+ * @param email 이메일
+ */
 public record MemberInfo(String name, String email) { }
