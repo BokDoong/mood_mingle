6523ad82a50ec7840efa136aee14dca1169992f3
이진 <ctce7226@gmail.com>
2024-05-10 15:41:49 +0900

    feat: 멤버 정보조회 API 설계

---
+++ src/main/java/uni/capstone/moodmingle/member/application/dto/response/MemberInfo.java
@@ -0,0 +1,3 @@
+package uni.capstone.moodmingle.member.application.dto.response;
+
+public record MemberInfo(String name, String email) { }
---
+++ src/main/java/uni/capstone/moodmingle/member/presentation/MemberController.java
@@ -0,0 +1,25 @@
+package uni.capstone.moodmingle.member.presentation;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import uni.capstone.moodmingle.member.application.MemberQueryService;
+import uni.capstone.moodmingle.member.application.dto.response.MemberInfo;
+
+/**
+ * 멤버 도메인 컨트롤러
+ *
+ * @author ijin
+ */
+@RestController
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberQueryService memberQueryService;
+
+    @GetMapping("/api/v1/member/{id}")
+    public MemberInfo getMemberInfo(@PathVariable Long id) {
+        return memberQueryService.findMember(id);
+    }
+}
