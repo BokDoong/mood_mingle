5652c85becbfe9627e55874f418985443c1467a8
이진 <ctce7226@gmail.com>
2025-03-26 13:33:16 +0900

    refactor: 로깅 핸들러 로직을 분리하여 GptClient 는 요청/응답 메세지에 관련된 로직만 담당하도록 책임 강화

---
+++ src/main/java/uni/capstone/moodmingle/clients/llm/gpt/log/LogHandlerService.java
@@ -0,0 +1,50 @@
+package uni.capstone.moodmingle.clients.llm.gpt.log;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.jboss.logging.MDC;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import uni.capstone.moodmingle.clients.llm.gpt.dto.GptMessage;
+
+import java.util.List;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class LogHandlerService {
+
+    private final FailedLogRepository failedLogRepository;
+
+    // 요청 메세지 로깅
+    public void logRequestMessages(List<GptMessage> messages) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("\n▶︎ GPT 요청 메세지\n");
+        sb.append(messages.toString());
+        log.info(sb.toString());
+    }
+
+    // 성공 메세지 로깅
+    public void logSuccessMessages(String response) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("\n🟢 OpenAI API 성공 응답 \n");
+        sb.append(response);
+        log.info(sb.toString());
+    }
+
+    // 실패 메세지 로깅, 저장
+    @Transactional
+    public void logAndSaveErrorMessages(Long diaryId, Throwable error) {
+        log.error("\n❌ OpenAI API 오류 응답: " + error.getMessage());
+        createAndSaveFailedLog(error.getMessage(), diaryId);
+    }
+
+    private void createAndSaveFailedLog(String log, Long diaryId) {
+        FailedLog failedLog = FailedLog.builder()
+                .threadId((String) MDC.get("requestId"))
+                .errorLog(log)
+                .diaryId(diaryId)
+                .build();
+        failedLogRepository.save(failedLog);
+    }
+}
